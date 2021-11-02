1268661cff1f38295544bde27c74dbfa4461c181
Swongi <91412645+Swongi@users.noreply.github.com>
2021-11-02 14:23:55 +0900

    Add files via upload

---
+++ live2d.js
@@ -0,0 +1,36 @@
+/*
+TN
+*/
+const cubism4Model =
+  "mosigang/mosigang.model3.json"; // 모델 설정
+
+const live2d = PIXI.live2d; 
+
+(async function main() {
+  const app = new PIXI.Application({
+    view: document.getElementById("canvas"),
+    autoStart: true,
+    resizeTo: window,
+    backgroundColor: 0xEAE3D1
+  });
+
+  const models = await Promise.all([
+    live2d.Live2DModel.from(cubism4Model)
+  ]);
+
+    models.forEach((model) => {
+    app.stage.addChild(model);
+
+    model.scale.set(0.7)
+    model.x = innerWidth * 0.4 - model.width/2;
+    model.y = innerHeight * 0.6 - model.height/2;
+  });
+  const model4 = models[0];
+
+  // handle tapping
+  model4.on("hit", (hitAreas) => {
+    if (hitAreas.includes("Car")) {
+      model4.motion("TapCar");
+    }
+  });
+})();
